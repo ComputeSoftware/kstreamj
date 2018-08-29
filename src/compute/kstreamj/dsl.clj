9a6519186160578dcded99f4bc44acb4e8a86c8a
dave <dave@computesoftware.com>
2018-08-29 11:37:18 -0700

    Get dispatch right for multi-arity functions

--- src/compute/kstreamj/dsl.clj
+++ src/compute/kstreamj/dsl.clj
@@ -4,456 +4,456 @@
            (org.apache.kafka.streams.kstream KStream Predicate Windows SessionWindows KGroupedStream
                                              TimeWindowedKStream SessionWindowedKStream KTable KGroupedTable)))
 
-(defmulti add-state-store! (fn [this & args] (type this)))
+(defmulti add-state-store! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod add-state-store! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti aggregate! (fn [this & args] (type this)))
+(defmulti aggregate! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod aggregate! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti branch! (fn [this & args] (type this)))
+(defmulti branch! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod branch! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti count! (fn [this & args] (type this)))
+(defmulti count! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod count! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti filter! (fn [this & args] (type this)))
+(defmulti filter! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod filter! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti filter-not! (fn [this & args] (type this)))
+(defmulti filter-not! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod filter-not! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti flat-map! (fn [this & args] (type this)))
+(defmulti flat-map! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod flat-map! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti flat-map-values! (fn [this & args] (type this)))
+(defmulti flat-map-values! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod flat-map-values! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti foreach! (fn [this & args] (type this)))
+(defmulti foreach! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod foreach! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti group-by! (fn [this & args] (type this)))
+(defmulti group-by! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod group-by! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti group-by-key! (fn [this & args] (type this)))
+(defmulti group-by-key! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod group-by-key! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti join! (fn [this & args] (type this)))
+(defmulti join! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod join! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti left-join! (fn [this & args] (type this)))
+(defmulti left-join! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod left-join! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti map! (fn [this & args] (type this)))
+(defmulti map! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod map! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti map-values! (fn [this & args] (type this)))
+(defmulti map-values! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod map-values! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti merge! (fn [this & args] (type this)))
+(defmulti merge! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod merge! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti outer-join! (fn [this & args] (type this)))
+(defmulti outer-join! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod outer-join! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti peek! (fn [this & args] (type this)))
+(defmulti peek! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod peek! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti print! (fn [this & args] (type this)))
+(defmulti print! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod print! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti process! (fn [this & args] (type this)))
+(defmulti process! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod process! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti reduce! (fn [this & args] (type this)))
+(defmulti reduce! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod reduce! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti stream! (fn [this & args] (type this)))
+(defmulti stream! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod stream! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti through! (fn [this & args] (type this)))
+(defmulti through! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod through! :default
   [this & args]
   (throw (UnsupportedOperationException.)))
 
-(defmulti to! (fn [this & args] (type this)))
+(defmulti to! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod to! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti to-stream! (fn [this & args] (type this)))
+(defmulti to-stream! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod to-stream! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti transform! (fn [this & args] (type this)))
+(defmulti transform! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod transform! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti transform-values! (fn [this & args] (type this)))
+(defmulti transform-values! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod transform-values! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
-(defmulti windowed-by! (fn [this & args] (type this)))
+(defmulti windowed-by! (fn [this & args] [(type this) (inc (count args))]))
 (defmethod windowed-by! :default
   [& _]
   (throw (UnsupportedOperationException.)))
 
 ;;; StreamsBuilder
 
-(defmethod stream! StreamsBuilder
+(defmethod stream! [StreamsBuilder 2]
   [builder topics]
   (.stream builder (if (string? topics) topics (vec topics))))
 
-(defmethod add-state-store! StreamsBuilder
+(defmethod add-state-store! [StreamsBuilder 2]
   [builder supplier]
   (.addStateStore builder supplier))
 
 ;;; KStream
 
-(defmethod filter! KStream
+(defmethod filter! [KStream 2]
   [stream pred]
   (.filter stream (u/->predicate pred)))
 
-(defmethod filter-not! KStream
+(defmethod filter-not! [KStream 2]
   [stream pred]
   (.filterNot stream (u/->predicate pred)))
 
-(defmethod map! KStream
+(defmethod map! [KStream 2]
   [stream kvm]
   (.map stream (u/->key-value-mapper kvm)))
 
-(defmethod map-values! KStream
+(defmethod map-values! [KStream 2]
   [stream vm]
   (.mapValues stream (u/->value-mapper vm)))
 
-(defmethod flat-map! KStream
+(defmethod flat-map! [KStream 2]
   [stream kvm]
   (.flatMap stream (u/->key-value-mapper kvm)))
 
-(defmethod flat-map-values! KStream
+(defmethod flat-map-values! [KStream 2]
   [stream vm]
   (.flatMapValues stream (u/->value-mapper vm)))
 
-(defmethod print! KStream
+(defmethod print! [KStream 2]
   [stream printed]
   (.print stream printed))
 
-(defmethod foreach! KStream
+(defmethod foreach! [KStream 2]
   [stream action]
   (.forEach stream (u/->for-each-action action)))
 
-(defmethod peek! KStream
+(defmethod peek! [KStream 2]
   [stream action]
   (.peek stream (u/->for-each-action action)))
 
-(defmethod branch! KStream
+(defmethod branch! [KStream 2]
   [stream preds]
   (vec (.branch stream (into-array Predicate (map u/->predicate preds)))))
 
-(defmethod merge! KStream
+(defmethod merge! [KStream 2]
   [stream s]
   (.merge stream s))
 
-(defmethod through! KStream
+(defmethod through! [KStream 2]
   [stream topic]
   (.through stream topic))
 
-(defmethod to! KStream
+(defmethod to! [KStream 2]
   [stream topic]
   (.to stream topic))
 
-(defmethod to! KStream
+(defmethod to! [KStream 3]
   [stream topic produced]
   (.to stream topic produced))
 
-(defmethod transform! KStream
+(defmethod transform! [KStream 2]
   [stream ts]
   (transform! stream ts []))
 
-(defmethod transform! KStream
+(defmethod transform! [KStream 3]
   [stream ts store-names]
-  (.transform stream (u/->transformer-supplier ts) store-names))
+  (.transform stream (u/->transformer-supplier ts) (into-array String store-names)))
 
-(defmethod transform-values! KStream
+(defmethod transform-values! [KStream 2]
   [stream vts]
   (transform-values! stream vts []))
 
-(defmethod transform-values! KStream
+(defmethod transform-values! [KStream 3]
   [stream vts store-names]
   (.transformValues stream (u/->value-transformer-supplier vts) (into-array String store-names)))
 
-(defmethod process! KStream
+(defmethod process! [KStream 2]
   [stream ps]
   (process! stream ps []))
 
-(defmethod process! KStream
+(defmethod process! [KStream 3]
   [stream ps store-names]
   (.process stream (u/->processor-supplier ps) (into-array String store-names)))
 
-(defmethod group-by-key! KStream
+(defmethod group-by-key! [KStream 1]
   [stream]
   (.groupByKey stream))
 
-(defmethod group-by-key! KStream
+(defmethod group-by-key! [KStream 2]
   [stream serialized]
   (.groupByKey stream serialized))
 
-(defmethod group-by! KStream
+(defmethod group-by! [KStream 2]
   [stream kvm]
   (.groupBy stream (u/->key-value-mapper kvm)))
 
-(defmethod group-by! KStream
+(defmethod group-by! [KStream 3]
   [stream kvm serialized]
   (.groupBy stream (u/->key-value-mapper kvm) serialized))
 
-(defmethod join! KStream
+(defmethod join! [KStream 4]
   [this stream-or-table value-joiner join-windows]
   (throw (UnsupportedOperationException. "Not implemented")))
 
-(defmethod join! KStream
+(defmethod join! [KStream 5]
   [this stream-or-table value-joiner join-windows joined]
   (throw (UnsupportedOperationException. "Not implemented")))
 
-(defmethod left-join! KStream
+(defmethod left-join! [KStream 4]
   [this stream-or-table value-joiner join-windows]
   (throw (UnsupportedOperationException. "Not implemented")))
 
-(defmethod left-join! KStream
+(defmethod left-join! [KStream 5]
   [this stream-or-table value-joiner join-windows joined]
   (throw (UnsupportedOperationException. "Not implemented")))
 
-(defmethod outer-join! KStream
+(defmethod outer-join! [KStream 4]
   [this stream value-joiner join-windows]
   (throw (UnsupportedOperationException. "Not implemented")))
 
-(defmethod outer-join! KStream
+(defmethod outer-join! [KStream 5]
   [this stream value-joiner join-windows joined]
   (throw (UnsupportedOperationException. "Not implemented")))
 
 ;;; KGroupedStream
 
-(defmethod count! KGroupedStream
+(defmethod count! [KGroupedStream 1]
   [stream]
   (.count stream))
 
-(defmethod count! KGroupedStream
+(defmethod count! [KGroupedStream 2]
   [stream materialized]
   (.count stream materialized))
 
-(defmethod reduce! KGroupedStream
+(defmethod reduce! [KGroupedStream 2]
   [stream r]
   (.reduce stream (u/->reducer r)))
 
-(defmethod reduce! KGroupedStream
+(defmethod reduce! [KGroupedStream 3]
   [stream r materialized]
   (.reduce stream (u/->reducer r) materialized))
 
-(defmethod aggregate! KGroupedStream
+(defmethod aggregate! [KGroupedStream 3]
   [stream init agg]
   (.aggregate stream (u/->initializer init) (u/->aggregator agg)))
 
-(defmethod aggregate! KGroupedStream
+(defmethod aggregate! [KGroupedStream 3]
   [stream init agg materialized]
   (.aggregate stream (u/->initializer init) (u/->aggregator agg) materialized))
 
-(defmethod windowed-by! KGroupedStream
+(defmethod windowed-by! [KGroupedStream 2]
   [stream windows]
   (.windowedBy stream windows))
 
 ;;; TimeWindowedKStream
 
-(defmethod count! TimeWindowedKStream
+(defmethod count! [TimeWindowedKStream 1]
   [stream]
   (.count stream))
 
-(defmethod count! TimeWindowedKStream
+(defmethod count! [TimeWindowedKStream 2]
   [stream materialized]
   (.count stream materialized))
 
-(defmethod reduce! TimeWindowedKStream
+(defmethod reduce! [TimeWindowedKStream 2]
   [stream r]
   (.reduce stream (u/->reducer r)))
 
-(defmethod reduce! TimeWindowedKStream
+(defmethod reduce! [TimeWindowedKStream 3]
   [stream r materialized]
   (.reduce stream (u/->reducer r) materialized))
 
-(defmethod aggregate! TimeWindowedKStream
+(defmethod aggregate! [TimeWindowedKStream 3]
   [stream init agg]
   (.aggregate stream (u/->initializer init) (u/->aggregator agg)))
 
-(defmethod aggregate! TimeWindowedKStream
+(defmethod aggregate! [TimeWindowedKStream 4]
   [stream init agg materialized]
   (.aggregate stream (u/->initializer init) (u/->aggregator agg) materialized))
 
 ;;; SessionWindowedKStream
 
-(defmethod count! SessionWindowedKStream
+(defmethod count! [SessionWindowedKStream 1]
   [stream]
   (.count stream))
 
-(defmethod count! SessionWindowedKStream
+(defmethod count! [SessionWindowedKStream 2]
   [stream materialized]
   (.count stream materialized))
 
-(defmethod reduce! SessionWindowedKStream
+(defmethod reduce! [SessionWindowedKStream 2]
   [stream r]
   (.reduce stream (u/->reducer r)))
 
-(defmethod reduce! SessionWindowedKStream
+(defmethod reduce! [SessionWindowedKStream 3]
   [stream r materialized]
   (.reduce stream (u/->reducer r) materialized))
 
-(defmethod aggregate! SessionWindowedKStream
+(defmethod aggregate! [SessionWindowedKStream 4]
   [stream init merg agg]
   (.aggregate stream (u/->initializer init) (u/->aggregator agg) (u/->merger merg)))
 
-(defmethod aggregate! SessionWindowedKStream
+(defmethod aggregate! [SessionWindowedKStream 5]
   [stream init agg merg materialized]
   (.aggregate stream (u/->initializer init) (u/->aggregator agg) (u/->merger merg) materialized))
 
 ;;; KTable
 
-(defmethod filter! KTable
+(defmethod filter! [KTable 2]
   [table pred]
   (.filter table (u/->predicate pred)))
 
-(defmethod filter! KTable
+(defmethod filter! [KTable 3]
   [table pred materialized]
   (.filter table (u/->predicate pred) materialized))
 
-(defmethod filter-not! KTable
+(defmethod filter-not! [KTable 2]
   [table pred]
   (.filterNot table (u/->predicate pred)))
 
-(defmethod filter-not! KTable
+(defmethod filter-not! [KTable 3]
   [table pred materialized]
   (.filterNot table (u/->predicate pred) materialized))
 
-(defmethod map-values! KTable
+(defmethod map-values! [KTable 2]
   [table vm]
   (.mapValues table (u/->value-mapper vm)))
 
-(defmethod map-values! KTable
+(defmethod map-values! [KTable 3]
   [table vm materialized]
   (.mapValues table (u/->value-mapper vm) materialized))
 
-(defmethod to-stream! KTable
+(defmethod to-stream! [KTable 1]
   [table]
   (.toStream table))
 
-(defmethod to-stream! KTable
+(defmethod to-stream! [KTable 2]
   [table kvm]
   (.toStream table (u/->key-value-mapper kvm)))
 
-(defmethod transform-values! KTable
+(defmethod transform-values! [KTable 3]
   [table vts store-names]
   (.transformValues table (u/->value-transformer-with-key-supplier vts) (into-array String store-names)))
 
-(defmethod transform-values! KTable
+(defmethod transform-values! [KTable 4]
   [table vts store-names materialized]
   (.transformValues table (u/->value-transformer-with-key-supplier vts) (into-array String store-names)))
 
-(defmethod group-by! KTable
+(defmethod group-by! [KTable 2]
   [table kvm]
   (.groupBy table (u/->key-value-mapper kvm)))
 
-(defmethod group-by! KTable
+(defmethod group-by! [KTable 3]
   [table kvm serialized]
   (.groupBy table (u/->key-value-mapper kvm) serialized))
 
-(defmethod join! KTable
+(defmethod join! [KTable 3]
   [this table value-joiner]
   (throw (UnsupportedOperationException.)))
 
-(defmethod join! KTable
+(defmethod join! [KTable 4]
   [this table value-joiner materialized]
   (throw (UnsupportedOperationException.)))
 
-(defmethod left-join! KTable
+(defmethod left-join! [KTable 3]
   [this table value-joiner]
   (throw (UnsupportedOperationException.)))
 
-(defmethod left-join! KTable
+(defmethod left-join! [KTable 4]
   [this table value-joiner materialized]
   (throw (UnsupportedOperationException.)))
 
-(defmethod outer-join! KTable
+(defmethod outer-join! [KTable 3]
   [this table value-joiner]
   (throw (UnsupportedOperationException.)))
 
-(defmethod outer-join! KTable
+(defmethod outer-join! [KTable 4]
   [this table value-joiner materialized]
   (throw (UnsupportedOperationException.)))
 
 ;;; KGroupedTable
 
-(defmethod count! KGroupedTable
+(defmethod count! [KGroupedTable 1]
   [table]
   (.count table))
 
-(defmethod count! KGroupedTable
+(defmethod count! [KGroupedTable 2]
   [table materialized]
   (.count table materialized))
 
-(defmethod reduce! KGroupedTable
+(defmethod reduce! [KGroupedTable 3]
   [table r-add r-sub]
   (.reduce table (u/->reducer r-add) (u/->reducer r-sub)))
 
-(defmethod reduce! KGroupedTable
+(defmethod reduce! [KGroupedTable 4]
   [table r-add r-sub materialized]
   (.reduce table (u/->reducer r-add) (u/->reducer r-sub) materialized))
 
-(defmethod aggregate! KGroupedTable
+(defmethod aggregate! [KGroupedTable 4]
   [table init agg-add agg-sub]
   (.aggregate table (u/->initializer init) (u/->aggregator agg-add) (u/->aggregator agg-sub)))
 
-(defmethod aggregate! KGroupedTable
+(defmethod aggregate! [KGroupedTable 5]
   [table init agg-add agg-sub materialized]
   (.aggregate table (u/->initializer init) (u/->aggregator agg-add) (u/->aggregator agg-sub) materialized))
--- test/compute/kstreamj_test.clj
+++ test/compute/kstreamj_test.clj
@@ -1,7 +1,131 @@
 (ns compute.kstreamj-test
   (:require [clojure.test :refer :all]
-            [compute.kstreamj :refer :all]))
+            [compute.kstreamj.core :as streams]
+            [compute.kstreamj.dsl :as dsl]
+            [franzy.serialization.nippy.serializers :as ns]
+            [franzy.serialization.nippy.deserializers :as nd]
+            [taoensso.timbre :as log])
+  (:import (org.apache.kafka.common.serialization Serde)
+           (org.apache.kafka.streams.state Stores)
+           (org.apache.kafka.streams.processor Punctuator Processor TimestampExtractor PunctuationType)
+           (org.apache.kafka.streams.kstream Transformer Materialized Windows TimeWindows)
+           (org.apache.kafka.streams KeyValue StreamsConfig)
+           (org.apache.kafka.clients.consumer ConsumerConfig ConsumerRecord)
+           (java.util.concurrent TimeUnit)))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(log/set-level! :error)
+
+(Thread/setDefaultUncaughtExceptionHandler
+  (reify Thread$UncaughtExceptionHandler
+    (uncaughtException [_ thread ex]
+      (println ex "Uncaught exception on" (.getName thread)))))
+
+(deftype NippySerde []
+  Serde
+  (configure [this map b])
+  (close [this])
+  (serializer [this]
+    (ns/nippy-serializer))
+  (deserializer [this]
+    (nd/nippy-deserializer)))
+
+(defn get-state
+  [context store-name k]
+  (-> context
+      (.getStateStore store-name)
+      (.get k)))
+
+(def state-store-name "state-store")
+(def state-store-builder (Stores/keyValueStoreBuilder (Stores/inMemoryKeyValueStore state-store-name)
+                                                      (NippySerde.) (NippySerde.)))
+
+(deftype SimulationTimestampExtractor []
+  TimestampExtractor
+  (^long extract [_ ^ConsumerRecord record ^long previousTimeStamp]
+    (let [v (.value record)]
+      (long (:time v)))))
+
+(deftype SimulationPunctuator [context]
+  Punctuator
+  (punctuate [_ timestamp]
+    (let [state-store (.getStateStore context "doink")
+          state (.all state-store)]
+      (println "------------- PUNCTUATE")
+      (doseq [s (iterator-seq state)]
+        (println
+          (.value s)
+          (-> s (.key) (.window) (.end) (java.util.Date.)))))))
+
+(deftype SimulationTransformer [^{:volatile-mutable true} context]
+  Transformer
+  (init [_ c]
+    (set! context c))
+  ;;; Schedule punctuation
+  (transform [_ k v]
+    (let [state-store (.getStateStore context state-store-name)
+          state (or (.get state-store k) {:count 10})
+          state' (assoc state :demand (rand-int 10))]
+      (.put state-store k state')
+      (KeyValue/pair k (merge state' v))))
+  (close [_]))
+
+(deftype ActionTransformer [^{:volatile-mutable true} context]
+  Transformer
+  (init [_ c]
+    (set! context c))
+  ;;; Schedule punctuation
+  (transform [_ k v]
+    (let [t (:time v)
+          action (:action v)
+          update-fn (condp = action :inc inc :dec dec)
+          state-store (.getStateStore context state-store-name)
+          state (or (.get state-store k) {})
+          state' (update state :count update-fn)]
+      (.put state-store k state')
+      (KeyValue/pair k state')))
+  (close [_]))
+
+(deftype DoinkProcessor [^{:volatile-mutable true} context]
+  Processor
+  (init [_ c]
+    (set! context c)
+    (.schedule context (.toMillis TimeUnit/MINUTES 30) PunctuationType/STREAM_TIME
+               (SimulationPunctuator. context)))
+  (process [_ k v])
+  (close [_]))
+
+
+(def kafka-config
+  {StreamsConfig/APPLICATION_ID_CONFIG            "example-consumer"
+   StreamsConfig/BOOTSTRAP_SERVERS_CONFIG         "localhost:9092"
+   StreamsConfig/CACHE_MAX_BYTES_BUFFERING_CONFIG 0
+   StreamsConfig/COMMIT_INTERVAL_MS_CONFIG        100000
+   StreamsConfig/DEFAULT_KEY_SERDE_CLASS_CONFIG   NippySerde
+   StreamsConfig/DEFAULT_VALUE_SERDE_CLASS_CONFIG NippySerde
+   ConsumerConfig/AUTO_OFFSET_RESET_CONFIG        "earliest"})
+
+(defn foo
+  [v]
+  (assoc v :action (if (> 5 (:demand v)) :dec :inc)))
+
+(def builder (streams/builder))
+
+(-> builder
+    (dsl/add-state-store! state-store-builder)
+    (dsl/stream! "simulation-ticks")
+    (dsl/transform! (SimulationTransformer. nil) [state-store-name])
+    (dsl/map-values! foo)
+    (dsl/transform! (ActionTransformer. nil) [state-store-name])
+    (dsl/group-by-key!)
+    (dsl/windowed-by! (TimeWindows/of (.toMillis TimeUnit/HOURS 1)))
+    (dsl/aggregate! (constantly 0)
+                    (fn [k v total] (if (:end v) total (inc total)))
+                    (Materialized/as "doink"))
+    (dsl/to-stream!)
+    (dsl/process! (DoinkProcessor. nil) ["doink"]))
+
+
+
+
+#_(def test-driver (TopologyTestDriver. (.build builder) (map->properties kafka-config)))
+#_(def crf (ConsumerRecordFactory. "topic-input" (nippy-serializers/nippy-serializer) (nippy-serializers/nippy-serializer)))
